aae38888a1032dca7dd71a4fb717b2591b566f4e
Daniel McInnis <dmcinnis@uci.edu>
2017-02-15 16:38:03 -0800

    Dynamically load map background image

--- js/script-results.js
+++ js/script-results.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	var location = sessionStorage.getItem('location');
-	var imageSource = 'https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=4&size=640x640&key=AIzaSyD4u8OfeiUVGO3leigttTSnvFSgDznwZtA';
-	$(".third-panel img").attr("src", imageSource);
+
+	var imageSource = `https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=11&size=960x960&key=AIzaSyD4u8OfeiUVGO3leigttTSnvFSgDznwZtA`;
+	
+	document.styleSheets[1].insertRule(`.first-panel-savings::after { background-image: url("${imageSource}"); }`, 0);
 });
