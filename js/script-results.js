a5ce131557ed6d39b7a495a9f49956cc2beab236
Daniel McInnis <dmcinnis@uci.edu>
2017-02-17 14:44:09 -0800

    capture start and destination inputs on button click, store in session

--- js/script-results.js
+++ js/script-results.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 	var location = sessionStorage.getItem('location');
+	var startAddress = sessionStorage.getItem('startAddress');
+	var destinationAddress = sessionStorage.getItem('destinationAddress');
+	// Should probably remove the start address and destination address after plugging them into API, that way we won't run into any weird errors when reassigning session variables
+	console.log(startAddress, destinationAddress);
 
 	var imageSource = `https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=11&size=960x960&key=AIzaSyD4u8OfeiUVGO3leigttTSnvFSgDznwZtA`;
 
