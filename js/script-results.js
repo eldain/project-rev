203260279a2363d3e74e1c0a07751bb7bc02ffcd
Daniel McInnis <dmcinnis@uci.edu>
2017-02-26 18:49:01 -0800

    hard coded both json files, makes life easier

--- js/script-results.js
+++ js/script-results.js
@@ -1,8 +1,58 @@
+// Hard coded car data. In ideal world, would pull car data from a live source, but is outside the scope of this project (and class) for the time being
+var gasVehicles = [
+	{
+	"manufacturer": "Honda",
+	"model": "Accord",
+	"msrp": 23000,
+	"mpg": 32,
+	"notes": "sedan"
+	}
+];
+var electricVehicles = [
+	{
+		"manufacturer": 'BMW',
+		"model": 'i3',
+		"msrp": 48000,
+		"mpg": 117,
+		"notes": 'SUV'
+	},
+	{
+		"manufacturer": 'Ford',
+		"model": 'Focus Electric',
+		"msrp": 29100,
+		"mpg": 107,
+		"notes": 'hatchback'
+	},
+	{
+		"manufacturer": 'Fiat',
+		"model": '500E',
+		"msrp": 33900,
+		"mpg": 115,
+		"notes": '(really) compact car'
+	},
+	{
+		"manufacturer": 'Nissan',
+		"model": 'Leaf',
+		"msrp": 36000,
+		"mpg": 113,
+		"notes": 'sedan'
+	},
+	{
+		"manufacturer": 'Tesla',
+		"model": 'S',
+		"msrp": 69200,
+		"mpg": 94,
+		"notes": 'luxury sedan'
+	}
+];
+
 $(document).ready(function(){
+	console.log(electricVehicles[0].model);
+
 	var startInputSavings = document.querySelector('.start-input-savings');
 	var destinationInputSavings = document.querySelector('.destination-input-savings');
 
-	console.log(startInputSavings, destinationInputSavings);
+	// console.log(startInputSavings, destinationInputSavings);
 
 	new google.maps.places.Autocomplete(startInputSavings);
 	new google.maps.places.Autocomplete(destinationInputSavings);
@@ -11,7 +61,7 @@ $(document).ready(function(){
 	var startAddress = sessionStorage.getItem('startAddress');
 	var destinationAddress = sessionStorage.getItem('destinationAddress');
 	// Should probably remove the start address and destination address after plugging them into API, that way we won't run into any weird errors when reassigning session variables
-	console.log(startAddress, destinationAddress);
+	// console.log(startAddress, destinationAddress);
 
 	function getDistance()
 	  {
@@ -36,7 +86,7 @@ $(document).ready(function(){
 	    });
 	  }
 
-		getDistance();
+		// getDistance();
 
 	var imageSource = `http://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=11&size=960x960&key=AIzaSyD4u8OfeiUVGO3leigttTSnvFSgDznwZtA`;
 
