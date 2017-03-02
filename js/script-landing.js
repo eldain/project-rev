69e2ee76814b5d21ea042f87e4f04f0a99ac12e0
Daniel McInnis <dmcinnis@uci.edu>
2017-03-02 13:49:49 -0800

    Submit button checks to see if there are two addresses input

--- js/script-landing.js
+++ js/script-landing.js
@@ -6,21 +6,29 @@ $(document).ready(function(){
 	new google.maps.places.Autocomplete(startInput);
 	new google.maps.places.Autocomplete(destinationInput);
 
-	function setLocation(e){
-		e.preventDefault();
-		sessionStorage.setItem('location', 'University+of+California+Irvine');
-		window.location.href = "/savings.html";
-	}
+	// This would grab mid point and pass it along for background image on next page, probably not worth the time and energy though
+	// function setLocation(e){
+	// 	e.preventDefault();
+	// 	sessionStorage.setItem('location', 'University+of+California+Irvine');
+	// }
 
-	function setAddresses(){
+	function setAddresses(e){
+		e.preventDefault();
 		var startAddress = startInput.value.split(", ").join("+");
 		var destinationAddress = destinationInput.value.split(", ").join("+");
 		sessionStorage.setItem('startAddress', startAddress);
 		sessionStorage.setItem('destinationAddress', destinationAddress);
 	}
 
+	function nextPageCheck(){
+		if(startInput.value === "" || destinationInput.value === ""){
+			submitRouteButton.classList.remove("bg-green");
+			submitRouteButton.classList.add("bg-red");
+		} else window.location.href = "/savings.html";
+	}
+
 	submitRouteButton.addEventListener('click', () => {
-		setLocation(event);
-		setAddresses();
+		setAddresses(event);
+		nextPageCheck();
 	});
 });
